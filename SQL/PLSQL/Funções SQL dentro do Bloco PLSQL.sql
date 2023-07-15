480fbc2a4a1f42f4c36f48d063f9731d8476a66f
Maria Eduarda Ciarini Moura <DudaCiarinii@gmail.com>
2023-07-14 23:17:35 -0300

    Renomeando e Formatando
    Comandos SQL/PLSQL

---
+++ SQL/PLSQL/Funções SQL dentro do Bloco PLSQL.sql
@@ -0,0 +1,22 @@
+-- Utilizando Funções SQL dentro de um Bloco PL/SQL
+
+SET SERVEROUTPUT ON
+
+DECLARE
+    VNUMERO1 NUMBER (13, 2); --  Declaração de variável para o Primero número
+    VNUMERO2 NUMBER (13, 2); --  Declaração de variável para o Segundo número
+    VMEDIA   NUMBER (13, 2); --  Declaração de variavél para receber a Media calculada
+BEGIN
+ /* Calculo do valor da média entre dois Números
+   Autor: Dudinha
+   Data: 14/07/23 */
+    VNUMERO1 := 50000;
+    VNUMERO2 := 5E3;
+    VMEDIA := ROUND((VNUMERO1 + VNUMERO2) / 2, 2); -- Calculo da Média entre os dois números
+    DBMS_OUTPUT.PUT_LINE('vnumero1 = '
+        || VNUMERO1); -- Impressão do Primeiro Número
+    DBMS_OUTPUT.PUT_LINE('vnumero2 = '
+        || VNUMERO2); -- Impressão do Segundo Número
+    DBMS_OUTPUT.PUT_LINE('Média = '
+        || TO_CHAR(VMEDIA, '99G99G999D99')); -- Impressão da Média Calculada
+END;
