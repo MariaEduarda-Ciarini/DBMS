3d973c92b33e2c9a63e3c529cc707237b28d9ed9
Maria Eduarda Ciarini Moura <DudaCiarinii@gmail.com>
2023-07-18 22:17:30 -0300

    ctrl + k ctrl +f
    apenas

--- SQL/PLSQL/Blocos Aninhados e Escopo de Identificadores.sql
+++ SQL/PLSQL/Blocos Aninhados e Escopo de Identificadores.sql
@@ -4,41 +4,38 @@
 SET SERVEROUTPUT ON
 
 DECLARE
-    vBloco1 VARCHAR2(20) := 'Bloco 1';
+    VBLOCO1 VARCHAR2(20) := 'Bloco 1';
 BEGIN
-    DBMS_OUTPUT.PUT_LINE('Refereciando variável do Bloco 1: ' 
-    || vBloco1);
-DECLARE
-    vBloco3 VARCHAR2(20) := 'Bloco 3';
-BEGIN
-    DBMS_OUTPUT.PUT_LINE('Refereciando variável do Bloco 1: ' 
-    || vBloco1);
-    DBMS_OUTPUT.PUT_LINE('Refereciando variável do Bloco 3: ' 
-    || vBloco3);
+    DBMS_OUTPUT.PUT_LINE('Refereciando variável do Bloco 1: '
+        || VBLOCO1);
+    DECLARE
+        VBLOCO3 VARCHAR2(20) := 'Bloco 3';
+    BEGIN
+        DBMS_OUTPUT.PUT_LINE('Refereciando variável do Bloco 1: '
+            || VBLOCO1);
+        DBMS_OUTPUT.PUT_LINE('Refereciando variável do Bloco 3: '
+            || VBLOCO3);
+    END;
+    DBMS_OUTPUT.PUT_LINE('Refereciando variável do Bloco 1: '
+        || VBLOCO1);
+ -- Se voce referencia vbloco 2 aqui ocorrera Error
 END;
-    DBMS_OUTPUT.PUT_LINE('Refereciando variável do Bloco 1: ' 
-    || vBloco1);
-    -- Se voce referencia vbloco 2 aqui ocorrera Error
-END;
-
-
--- Identifcando Blocos através de Labels
-
-<<BLOCO1>>
+ -- Identifcando Blocos através de Labels
+ <<BLOCO1>>
 DECLARE
-    vBloco1 VARCHAR2(20) := 'Bloco 1';
+    VBLOCO1 VARCHAR2(20) := 'Bloco 1';
 BEGIN
-    DBMS_OUTPUT.PUT_LINE('Refereciando variável do Bloco 1: ' 
-    || bloco1.vBloco1);
-<<BLOCO2>>
-DECLARE
-    vBloco2 VARCHAR2(20) := 'Bloco 2';
-BEGIN
-     DBMS_OUTPUT.PUT_LINE('Refereciando variável do Bloco 1: ' 
-    || bloco1.vBloco1);
-     DBMS_OUTPUT.PUT_LINE('Refereciando variável do Bloco 1: ' 
-    || bloco2.vBloco2);
-END;
-     DBMS_OUTPUT.PUT_LINE('Refereciando variável do Bloco 1: ' 
-    || bloco1.vBloco1);
+    DBMS_OUTPUT.PUT_LINE('Refereciando variável do Bloco 1: '
+        || BLOCO1.VBLOCO1);
+    <<BLOCO2>>
+    DECLARE
+        VBLOCO2 VARCHAR2(20) := 'Bloco 2';
+    BEGIN
+        DBMS_OUTPUT.PUT_LINE('Refereciando variável do Bloco 1: '
+            || BLOCO1.VBLOCO1);
+        DBMS_OUTPUT.PUT_LINE('Refereciando variável do Bloco 1: '
+            || BLOCO2.VBLOCO2);
+    END;
+    DBMS_OUTPUT.PUT_LINE('Refereciando variável do Bloco 1: '
+        || BLOCO1.VBLOCO1);
 END;
