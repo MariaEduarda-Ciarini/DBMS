7ba95568f1889606de700d94ed65b67bf9defc7c
Maria Eduarda Ciarini Moura <DudaCiarinii@gmail.com>
2023-08-02 18:43:57 -0300

    Add New Class
    Nova aula sobre o assunto..

---
+++ SQL/PLSQL/Cursor Implicito.sql
@@ -0,0 +1,16 @@
+-- Aula sobre Cursor Implícito
+SET SERVEROUTPUT ON 
+    DECLARE
+        vDEPARTMENT_ID EMPLOYEES.DEPARTMENT_ID%TYPE := 60;
+        vPERCENTUAL  NUMBER(3) :=10;
+    BEGIN
+        UPDATE EMPLOYEES
+        SET SALARY = SALARY * (1 + VPERCENTUAL / 100)
+        WHERE DEPARTMENT_ID = vDEPARTMENT_ID;
+        
+        DBMS_OUTPUT.PUT_LINE('Numero de Empregados atualizados: '
+        || SQL%ROWCOUNT);
+    END;
+    /
+
+ROLLBACK;
