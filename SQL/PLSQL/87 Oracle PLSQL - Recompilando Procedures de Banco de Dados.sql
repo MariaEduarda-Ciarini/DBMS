85d368af441334f2b8416ea18e5b6634f2ba5213
MariaEduarda-Ciarini <DudaCiarinii@gmail.com>
2024-09-18 20:07:38 -0300

    87. Oracle PL/SQL - Recompilando Procedures de Banco de Dados
    87. Oracle PL/SQL - Recompilando Procedures de Banco de Dados

---
+++ SQL/PLSQL/87 Oracle PLSQL - Recompilando Procedures de Banco de Dados.sql
@@ -0,0 +1,15 @@
+--
+--
+-- Seção 14 - Procedures de Banco de Dados
+--
+-- Aula 5 - Recompilando Procedures de Banco de Dados
+--
+
+-- Recompilando Procedures de Banco de Dados
+
+ALTER PROCEDURE PRC_INSERE_EMPREGADO COMPILE;
+
+
+
+
+
